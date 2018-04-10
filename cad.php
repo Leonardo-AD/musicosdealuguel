930b0e1d8bb6b380826e065f23f1c19fec61eba4
Anderson-Aslap <aslap7xx@gmail.com>
2018-04-10 10:08:33 -0300

    CRud

---
+++ cad.php
@@ -0,0 +1,19 @@
+<?php
+
+include 'init.php';
+
+$newu = $_POST['novo_usuario'];
+$newp = $_POST['nova_senha'];
+$user_new = $newu."-".$newp;
+
+$users = file("users-pws.txt");
+$users[] = $user_new."\n";
+$users_string = implode('', $users);
+
+file_put_contents('users-pws.txt', $users_string);
+
+echo "usuario cadastrado";
+
+?>
+
+<a href="login.php">Acessar</a>
---
+++ piu.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Documento</title>
+</head>
+<body>
+<h2>Digite algumas infomações</h2>
+<form>
+	<input type="text" name="nome" placeholder="Digite seu nome"><br><br>
+	<input type="text" name="genero_musical" placeholder="Digite seu gênero musical"><br><br>
+	<input type="submit" name="enviar">
+</form>
+</body>
+</html>
