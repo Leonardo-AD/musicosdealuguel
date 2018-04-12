096675ef7eb53dd23127a989f4d838c870061af0
Leonardo-AD <leonardo.alves779@gmail.com>
2018-04-12 12:00:05 -0300

    corrigindo erros de português

--- piu.php
+++ piu.php
@@ -6,12 +6,12 @@
 	<link rel="stylesheet" type="text/css" href="estilo.css">
 </head>
 <body>
-<h2 id="logado">Digite algumas infomações</h2>
+<h2 id="logado">Digite algumas informações</h2>
 <form id="logado" action="add.php" method="POST">
 	<input type="text" name="nome" placeholder="Digite seu nome"><br><br>
 	<input type="text" name="genero_musical" placeholder="Digite seu gênero musical"><br><br>
 	<input id="envi" type="submit" value="enviar">
-</form><br>
+</form>
 <a id="sair" href="logout.php">Sair</a>
 </body>
 </html>
